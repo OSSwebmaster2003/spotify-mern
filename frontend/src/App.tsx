0263683e7b2a39b90d06f80777d6739edf7324d7
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2025-03-03 22:05:11 -0800

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-lg text-red-600">Tailwind working</h1>
+      <Button variant="outline">Shandcn Button</Button>
+    </div>
+  )
+}
+
+export default App
