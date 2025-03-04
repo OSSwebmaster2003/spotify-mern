6c507b6e9cf923b7d5d0ad688f7064f917c3c567
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2025-03-04 02:16:59 -0800

    Frontend Auth setup finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import { Button } from "./components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-lg text-red-600">Tailwind working</h1>
-      <Button variant="outline">Shandcn Button</Button>
-    </div>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   )
 }
 
