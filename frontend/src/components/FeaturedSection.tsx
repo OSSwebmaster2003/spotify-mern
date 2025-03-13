e22fccf85569528ba397979a94ab940cfdca9841
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2025-03-13 03:05:48 -0700

    play button

--- frontend/src/components/FeaturedSection.tsx
+++ frontend/src/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import { useMusicStore } from "@/stores/useMusicStore";
 import FeaturedGridSkeleton from "./skeletons/FeaturedGridSkeleton";
+import PlayButton from "./PlayButton";
 
 const FeaturedSection = () => {
   const { featuredSongs, loading, error } = useMusicStore();
@@ -24,9 +25,9 @@ const FeaturedSection = () => {
             <p className="font-medium truncate">{song.title}</p>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
           </div>
-        </div>
 
-        // play button
+          <PlayButton song={song} />
+        </div>
       ))}
     </div>
   );
--- frontend/src/components/SectionGrid.tsx
+++ frontend/src/components/SectionGrid.tsx
@@ -1,6 +1,7 @@
 import { ISong } from "@/types";
 import SectionGridSkeleton from "./skeletons/SectionGridSkeleton";
 import { Button } from "./ui/button";
+import PlayButton from "./PlayButton";
 
 interface ISectionGrid {
   loading?: boolean;
@@ -35,8 +36,8 @@ const SectionGrid = ({ title, songs, loading }: ISectionGrid) => {
                   alt={song.title}
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
                 />
-                {/* play button here  */}
               </div>
+              <PlayButton song={song} />
             </div>
             <h3 className="font-medium mb-2 truncate">{song.title}</h3>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
